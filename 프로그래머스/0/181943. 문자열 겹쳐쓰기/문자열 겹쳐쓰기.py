9cb2e918eec1e2ac9e7bdf778542b9638c401fb9
swylab <dndud05066@gmail.com>
2024-11-19 23:05:24 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.44 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -0,0 +1,13 @@
+def solution(m, o, s):
+    answer=[]
+    for i in range(len(m)):
+        if i < s:
+            answer.append(m[i])
+        elif i >= s + len(o):
+            answer.append(m[i])
+        else:
+            answer.append(o[i-s])
+    a = "".join(answer)
+    print(a)
+    return a
+        
