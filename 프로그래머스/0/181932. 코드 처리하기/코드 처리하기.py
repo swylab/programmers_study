fb61b90c49f916d885c0e58d7e0b190410289987
swylab <dndud05066@gmail.com>
2025-09-09 21:33:46 +0900

    [level 0] Title: 코드 처리하기, Time: 15.84 ms, Memory: 9.44 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,23 @@
+def solution(code):
+    mode = 0
+    idx = 0
+    ret = ""
+    for idx in range(len(code)):
+        # mode가 0일때
+        if mode == 0:
+            if code[idx] == "1":
+                mode = 1
+            else:
+                if idx%2 == 0:
+                    ret=ret+code[idx]
+        # mode가 1일때
+        else:
+            if code[idx] == "1":
+                mode = 0
+            else:
+                if idx%2 == 1:
+                    ret=ret+code[idx]
+    idx=+1
+    if len(ret) == 0:
+        return "EMPTY"
+    return ret
