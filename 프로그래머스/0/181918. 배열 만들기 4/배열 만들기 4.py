ad1be689339319a6cac8cc975575e71d1041e920
swylab <dndud05066@gmail.com>
2025-09-09 23:17:52 +0900

    [level 0] Title: 배열 만들기 4, Time: 49.61 ms, Memory: 13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,18 @@
+def solution(arr):
+    stk = []
+    i = 0
+    if len(stk)==0:
+        stk.append(arr[i])
+        i+=1
+        
+    while i < len(arr):
+        if len(stk)==0:
+            stk.append(arr[i])
+            i+=1
+        elif stk[-1] < arr[i]:
+            stk.append(arr[i])
+            i+=1
+        else:
+            stk.pop()
+            
+    return stk
