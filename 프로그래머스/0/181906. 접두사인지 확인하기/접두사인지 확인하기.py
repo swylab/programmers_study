0743184ae9b3320d863d85d63cb50380a3b829e2
swylab <dndud05066@gmail.com>
2025-09-16 23:27:38 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -0,0 +1,9 @@
+def solution(my_string, is_prefix):
+    answer = 0
+    all_prefix = []
+    for i in range(len(my_string)):
+        all_prefix.append(my_string[:i])
+    if is_prefix in all_prefix:
+        return 1
+    else:
+        return 0
