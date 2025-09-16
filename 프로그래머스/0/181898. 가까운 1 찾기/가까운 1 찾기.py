61c3c0a1ea41fcdd39b07996c46e5a59d72f05fd
swylab <dndud05066@gmail.com>
2025-09-17 00:17:27 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.01 ms, Memory: 9.83 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,8 +1,7 @@
 def solution(arr, idx):
+    answer = -1
     for i in range(idx, len(arr)):
         if arr[i] == 1:
             answer = i 
             break
-        elif arr[i] == 0:
-            answer = -1
     return answer
