f8319bfb60f6c0c6aeae7a0b52f3a2bf9158b11d
swylab <dndud05066@gmail.com>
2025-09-17 19:40:33 +0900

    [level 0] Title: 리스트 자르기, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
@@ -1,17 +1,12 @@
 def solution(n, slicer, num_list):
-    answer = []
+    a = slicer[0]
+    b = slicer[1]
+    c = slicer[2]
     if n == 1:
-        for i in range(0, slicer[1]+1):
-            answer.append(num_list[i])
+        return num_list[0:b+1]
     elif n == 2:
-        for i in range(slicer[0], len(num_list)):
-            answer.append(num_list[i])
+        return num_list[a:]
     elif n == 3:
-        for i in range(slicer[0], slicer[1]+1):
-            answer.append(num_list[i])
+        return num_list[a:b+1]
     elif n == 4:
-        for i in range(slicer[0], slicer[1]+1, slicer[2]):
-            answer.append(num_list[i])
-    return answer
-            
-    return answer
+        return num_list[a:b+1:c]
